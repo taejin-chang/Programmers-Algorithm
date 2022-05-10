9f8c214c8abd26cbabc791f0ab425f7d5f72fa62
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-10 14:03:52 +0900

    fix_HateSameNumber code

--- src/Practice/HateSameNumber.java
+++ src/Practice/HateSameNumber.java
@@ -1,41 +1,23 @@
 package Practice;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class HateSameNumber {
     public int[] solution(int []arr) {
-        int[] answer = {};
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
+        List<Integer> list = new ArrayList<>();
         int value=-1;
-
-        for(int num : arr) {
-            if(num != value) {
-                list.add(num);
-                value = num;
+        for(int num : arr) {    //배열을 숫자로 하나씩 꺼낸다.
+            if(num != value) {  //리스트에 들어갈 숫자에 대한 중복 검증
+                list.add(num);  //리스트에 배열에서 꺼낸 숫자를 넣는다. (순서유지를 위해 Arraylist사용)
+                value = num;    //리스트에 넣은 숫자를 중복검증용으로 재 정의
             }
         }
-
-        for(int answers : list) {
-            answer = new int[]{answers};
-/*            for(int j=0;j<list.size();j++) {
-                answer[j] = answers;
-            }*/
+        int[] answer = new int[list.size()]; //리스트의 배열크기와 같은 배열 생성
+        for(int j=0;j<list.size();j++) {
+            answer[j] = list.get(j);         //리스트에서 인덱스 순서대로 int배열에 담는다.
         }
 
-        /*for(int j=0;j<list.size();j++) {
-            answer[j] = (int) list.get(j);
-        }*/
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
         return answer;
-    }
-
-    public static void main(String[] args) {
-        new HateSameNumber().solution(new int[]{1, 2, 3, 3, 4});
     }
 }
