8ab3969602059851347fad00efa5b3c1205c4883
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-11 23:47:05 +0900

    add_solve mathpowCheck

---
+++ src/Practice/mathpowCheck.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class mathpowCheck {
+
+    public long solution(long n) {
+        long answer = 0;
+        while(true) {
+            for(long i=0;i<n;i++) {
+                if(n==i*i) {
+                    answer = (long)Math.pow(i+1,2);
+                    break;
+                } else if (i*i>n){
+                    answer = -1;
+                }
+            }
+            break;
+        }
+        return answer;
+    }
+
+/*    public long solution(long n) {
+        if (Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        }
+
+        return -1;
+    }*/
+}
