7ff2a9b8e2dbfc1644becf2f7bd2822cf93c2ea2
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-14 13:14:47 +0900

    add_solve reverseArray

---
+++ src/Practice/reverseArray.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class reverseArray {
+    public int[] solution(long n) {
+        String k = String.valueOf(n);
+        int[] answer = new int[k.length()];
+        char[] temp = new char[k.length()];
+
+
+        for(int i=0 ; i<k.length() ; i++ ) {
+            temp[i] = k.charAt(i);
+        }
+
+        for (int j=0 ; j<temp.length ; j++) {
+            answer[j] = Integer.parseInt(String.valueOf(temp[temp.length-1-j]));
+        }
+        return answer;
+    }
+}
