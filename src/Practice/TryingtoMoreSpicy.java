387e94be7adb348c471a3069bd3399bd681a2bdb
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-22 14:29:42 +0900

    add_Trying to use Heap at MoreSpicy

---
+++ src/Practice/TryingtoMoreSpicy.java
@@ -0,0 +1,17 @@
+package Practice;
+/*
+* K == Mix = Min + (Min2 * 2)
+*
+*
+*
+* */
+public class TryingtoMoreSpicy {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
