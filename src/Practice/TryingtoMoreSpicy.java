76189c4703511a9ff3aa06ac0b64da3012190407
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-27 23:07:22 +0900

    add_trying to solving moreSpicy

--- src/Practice/TryingtoMoreSpicy.java
+++ src/Practice/TryingtoMoreSpicy.java
@@ -1,4 +1,8 @@
 package Practice;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /*
 * K == Mix = Min + (Min2 * 2)
 *
@@ -8,10 +12,22 @@ package Practice;
 public class TryingtoMoreSpicy {
     public int solution(int[] scoville, int K) {
         int answer = 0;
+
+        List<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < scoville.length; i++) {
+            queue.add(scoville[i]);
+        }
+        queue.sort((a, b) -> a - b);
+        while (queue.get(0) < K) {
+            if (queue.size() < 2) return -1;
+            int f1 = queue.remove(0);
+            int f2 = queue.remove(0);
+            int newFood = f1 + (f2 * 2);
+            queue.add(newFood);
+            answer++;
+            queue.sort((a, b) -> a - b);
+        }
+
         return answer;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
