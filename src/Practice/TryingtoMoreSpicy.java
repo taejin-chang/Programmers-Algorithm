336be8b2e1520875d874ccda042cf0932ecca47c
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-05-28 23:00:06 +0900

    add_solution ver2 of moreSpicy

--- src/Practice/TryingtoMoreSpicy.java
+++ src/Practice/TryingtoMoreSpicy.java
@@ -1,7 +1,6 @@
 package Practice;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /*
 * K == Mix = Min + (Min2 * 2)
@@ -30,4 +29,33 @@ public class TryingtoMoreSpicy {
 
         return answer;
     }
+
+    public int solution2(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> heap = new PriorityQueue();
+
+        for (int aScoville : scoville) {
+            heap.offer(aScoville);
+        }
+
+        while (heap.peek() <= K) {
+            if (heap.size() == 1) {
+                return -1;
+            }
+            int a = heap.poll();
+            int b = heap.poll();
+
+
+            int result = a + (b * 2);
+
+            heap.offer(result);
+            answer ++;
+        }
+        return answer;
+    }
+
 }
+
+
+
+
