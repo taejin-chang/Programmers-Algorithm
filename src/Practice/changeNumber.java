d06191f0b12adb3485f82a550607a9f8a76723ce
suonini <93498907+taejin-chang@users.noreply.github.com>
2022-06-04 17:27:17 +0900

    add_solve changeNumber

---
+++ src/Practice/changeNumber.java
@@ -0,0 +1,10 @@
+package Practice;
+
+public class changeNumber {
+        public int solution(String s) {
+            int answer = 0;
+
+            answer = Integer.parseInt(s);
+            return answer;
+        }
+}
